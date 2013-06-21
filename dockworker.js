5bd80de1b0028ba9f96cd84d49be4f5fef2ba0df
Praful Rana <prafulrana@gmail.com>
2013-06-21 14:24:53 -0600

    Update dockworker.js

--- dockworker.js
+++ dockworker.js
@@ -1,52 +1,28 @@
 #!/usr/bin/env node
-/*
 var spawn = require('child_process').spawn;
-var fs = require('fs');
-var runnableStartCmd = process.env.RUNNABLE_START_CMD;
-var cmd = runnableStartCmd.shift();
-var args = runnableStartCmd;
-var runnableServiceCommands = process.env.RUNNABLE_SERVICE_CMDS;
-
-runnableServiceCommands.split(';').forEach(function (cmd) {
-  spawn(cmd);
-});
-
-var node = spawn(cmd, [args], { stdio: 'inherit' });
-
-
-  var bash = spawn('bash', [], {stdio: 'inherit' });
-  setInterval(function () {
-    process.stdin.write('.');
-  }, 1000);
-  var http = require('http');
-  http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-  }).listen(80);
-  console.log('Hello Kitty');
-*/
-var spawn = require('child_process').spawn;
-//var fs = require('fs');
 
 var serviceSrcDir = process.env.RUNNABLE_USER_DIR;
 var serviceLauncher = process.env.RUNNABLE_START_CMD.split(" ");
 var runnableServiceCommands = process.env.RUNNABLE_SERVICE_CMDS;
+var cmd = serviceLauncher.shift();
+var args = serviceLauncher;
 
-var cmd = serviceLauncher[0];
-var args = [];
+// Launch Services
 
-for (var i = 1; i < serviceLauncher.length; i++) {
-  args.push(serviceLauncher[i]);
-}
+runnableServiceCommands.split(';').forEach(function (commandLine) {
+  var commandArray = commandLine.split(" ");
+  var binary = commandArray.shift();
+  var binaryArgs = commandAray;
+  spawn(binary, [binaryArgs]);
+});
+
+// Launch our App
+
+var node = spawn(cmd, [args], { stdio: 'inherit', cwd: serviceSrcDir});
+
+// Debug only
 
 console.log(serviceSrcDir);
 console.log(cmd);
 console.log(args);
 console.log(runnableServiceCommands);
-
-runnableServiceCommands.split(';').forEach(function (cmd) {
-  spawn(cmd);
-});
-
-
-var node = spawn(cmd, [args], { stdio: 'inherit', cwd: serviceSrcDir});
