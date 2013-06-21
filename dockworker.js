125fc71b523c4588f4dc1593e1343a41781f5bc5
Praful Rana <prafulrana@gmail.com>
2013-06-21 14:36:55 -0600

    typo

--- dockworker.js
+++ dockworker.js
@@ -16,7 +16,7 @@ runnableServiceCommands.split(';').forEach(function (commandLine) {
   var commandArray = commandLine.split(" ");
   var log = fs.createWriteStream("/var/log/" + commandArray.join("_") + ".log");
   var binary = commandArray.shift();
-  var binaryArgs = commandAray;
+  var binaryArgs = commandArray;
   spawn(binary, [binaryArgs], function (proc) {
     proc.stdout.pipe(log);
     proc.stderr.pipe(log);
