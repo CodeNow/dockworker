ab47d04a896d654bd6c53033a10d829519f44ca9
Henry Allen-Tilford <htilford@gmail.com>
2013-06-20 19:31:59 -0600

    Update dockworker.js

--- dockworker.js
+++ dockworker.js
@@ -13,5 +13,5 @@ runnableServiceCommands.split(';').forEach(function (cmd) {
 var node = spawn(cmd, [args], { stdio: 'inherit' });*/
 var bash = spawn('bash', [], {stdio: 'inherit' });
 setInterval(function () {
-  process.stdout.write('.');
+  bash.stdin.write('echo foo\r\n');
 }, 100); 
