19bfb35707bd23384884c45545ce825b5799e93e
tjmehta <tj@init.me>
2013-11-01 19:28:08 -0700

    fixed tests

--- tests/commandTest.js
+++ tests/commandTest.js
@@ -1,6 +1,6 @@
 var request = require('request');
-process.env.RUNNABLE_START_CMD = 'npm start'; 
-process.env.RUNNABLE_USER_DIR = __dirname + '/fixtures'; 
+process.env.RUNNABLE_START_CMD = 'npm start';
+process.env.RUNNABLE_USER_DIR = __dirname + '/fixtures';
 process.env.RUNNABLE_SERVICE_CMDS = 'sleep 1000;sleep 1000';
 require('..');
 
@@ -24,8 +24,7 @@ describe('Cmd', function (){
       request.post({
         url: 'http://localhost:15000/api/cmd',
         json: {
-          cmd: 'node',
-          args: ['server.js']
+          cmd: 'node server.js',
         }
       }, function (err, res, body) {
         if (err) {
