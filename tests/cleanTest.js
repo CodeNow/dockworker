f2234bfa4578a81ccb5d24535b76b3338b5e9b05
generalhenry <htilford@gmail.com>
2013-12-09 18:20:34 -0800

    default swap

--- tests/cleanTest.js
+++ tests/cleanTest.js
@@ -1,18 +1,43 @@
 var request = require('request');
+var async = require('async');
 describe('Clean', function () {
-  it('should be dirty', function (done) {
+  it('should be clean', function (done) {
     request({
       url: 'http://localhost:15000/api/clean',
       json: {}
     }, function (err, res, body) {
       if (err) {
         done(err);
-      } else if (body !== true) {
+      } else if (body !== false) {
         done(new Error());
       } else {
         done();
       }
     });
+  });
+  it('should dirty on change', function (done) {
+    async.series([
+      function (cb) {
+        request.post({
+          url: 'http://localhost:15000/api/buildCmd',
+          json: 'npm run build'
+        }, cb);
+      },
+      function (cb) {
+        request({
+          url: 'http://localhost:15000/api/clean',
+          json: {}
+        }, function (err, res, body) {
+          if (err) {
+            cb(err);
+          } else if (body !== true) {
+            cb(new Error());
+          } else {
+            cb();
+          }
+        });
+      }
+    ], done);
   });
   it('should allow clean up', function (done) {
     request.post({
