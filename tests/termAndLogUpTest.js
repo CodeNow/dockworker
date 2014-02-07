feed493a7363d56717949ba85f071e9483251778
tjmehta <tj@init.me>
2014-02-06 17:53:29 -0800

    logUpChecker

--- tests/termAndLogUpTest.js
+++ tests/termAndLogUpTest.js
@@ -13,3 +13,17 @@ describe('checkTermUp', function () {
     });
   });
 });
+
+describe('checkLogUp', function () {
+  it('should respond 200', function (done) {
+    request('http://localhost:15000/api/checkLogUp', function (err, res, body) {
+      if (err) {
+        done(err);
+      } else if (res.statusCode !== 200) {
+        done(new Error(res.body));
+      } else {
+        done();
+      }
+    });
+  });
+});
