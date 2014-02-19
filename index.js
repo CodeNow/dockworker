afd036c5b9c7e48cfc8b203a1d03c7b378bc9663
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-02-18 16:11:43 -0800

    add a child killer

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ require('./lib/controllers/dockCleaner');
 require('./lib/controllers/serviceCommands');
 require('./lib/app');
 require('./lib/server').listen(15000);
+var childKiller = require('./lib/controllers/childKiller.js');
 
-process.on('SIGTERM', process.exit);
+process.on('SIGTERM', function() {
+	childKiller(process.getgid());
+	process.exit();
+});
