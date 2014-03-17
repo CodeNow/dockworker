dafaa1da3d0a837efff963cd95514c7877f6221f
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-03-17 01:10:32 -0700

    debug

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require('./lib/controllers/serviceCommands');
 require('./lib/app');
 require('./lib/server').listen(15000);
 var childKiller = require('./lib/controllers/childKiller.js');
-
+var debug = require('./lib/controllers/debugAdder.js');
+debug.addForProcess(process, "main process");
 process.on('SIGTERM', function() {
 	childKiller(process.pid, function() {
 		process.exit();
