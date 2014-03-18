77355d15dd1711cca6caac43995ad4dd3beee923
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-03-17 19:02:17 -0700

    removing child killer

--- tests/shoeTest.js
+++ tests/shoeTest.js
@@ -22,6 +22,41 @@ describe('Streams', function () {
         stream.write('echo $RUNNABLE_START_CMD\n');
       }
     });
+    it('should run another terminal with long running process', function (done) {
+      var count = 0;
+      var stream = new ShoeClient('ws://localhost:15000/streams/terminal');
+      var muxDemux = new MuxDemux(onStream);
+      stream.pipe(muxDemux).pipe(stream);
+      function onStream(stream) {
+        if (stream.meta === 'terminal') {
+          onTerminal(stream);
+        }
+      }
+      function onTerminal(stream) {
+        stream.write("sleep 10&");
+        done();
+      }
+    });
+    if (process.platform !== 'darwin') {
+      it('should only have 1 tail process', function (done) {
+        var exec = require('child_process').exec;
+        exec('ps -e -o command | grep -v grep | grep tail | wc -l',
+          function (error, stdout, stderr) {
+          console.log("stdout"+stdout);
+          if (parseInt(stdout) === 1) {
+            done();
+          } else {
+            // there is something going one
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            if (error !== null) {
+              console.log('exec error: ' + error);
+            }
+            done(new Error('child process still alive'));
+          }
+        });
+      });
+    }
     it('should have web down', function (done) {
       request('http://localhost:15000/api/checkWebUp', function (err, res, body) {
         if (err) {
